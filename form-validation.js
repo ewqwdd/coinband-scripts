501d7772f4ff5196faac00e18b69b8cbb4b0af67
Mykolai Skydan <117273969+ewqwdd@users.noreply.github.com>
2025-02-13 08:21:34 +0100

    Update form-validation.js

--- form-validation.js
+++ form-validation.js
@@ -452,7 +452,7 @@ const initForm = (form_) => {
       const input = elem.querySelector("input, textarea, select");
       const error = elem.querySelector(".invalid-msg");
 
-      if (elem.hasAttribute("data-input-max") && input?.length > Number(elem.getAttribute("data-input-max"))) {
+      if (elem.hasAttribute("data-input-max") && input?.value.length > Number(elem.getAttribute("data-input-max"))) {
         error.style.display = "flex";
         error.textContent = `Max length is ${elem.getAttribute("data-input-max")}`;
         check = false;
